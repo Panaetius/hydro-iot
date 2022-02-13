623b72edb68e276e30c5b79df5dcae2e9ee0fef5
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2022-02-13 13:19:34 +0100

    add logging to message queue gateway

--- hydro_iot/infrastructure/rabbitmq_gateway.py
+++ hydro_iot/infrastructure/rabbitmq_gateway.py
@@ -10,6 +10,7 @@ from hydro_iot.domain.config import IConfig
 from hydro_iot.domain.ph import PH
 from hydro_iot.domain.pressure import Pressure
 from hydro_iot.domain.temperature import WaterTemperature
+from hydro_iot.services.ports.logging import ILogging
 from hydro_iot.services.ports.message_queue import (
     IMessageQueuePublisher,
     IMessageQueueSubscriber,
@@ -18,9 +19,15 @@ from hydro_iot.services.ports.message_queue import (
 
 class RabbitMQGateway(IMessageQueuePublisher):
     config: IConfig = inject.attr(IConfig)
+    logging: ILogging = inject.attr(ILogging)
     publish_lock: Lock = Lock()
 
     def __init__(self) -> None:
+        self.logging.info("Setting up message queues")
+
+        self.sensor_data_channel = None
+        self.event_data_channel = None
+
         self.consume_connection = pika.SelectConnection(
             pika.ConnectionParameters(
                 host=self.config.message_queue_connection.host,
@@ -43,11 +50,12 @@ class RabbitMQGateway(IMessageQueuePublisher):
             on_open_callback=self.publish_connection_open_callback,
         )
         self.channels = dict()
-
+        self.logging.info("Starting messagequeue io loop")
         Thread(target=self.publish_connection.ioloop.start).start()
         Thread(target=self.consume_connection.ioloop.start).start()
 
     def publish_connection_open_callback(self, connection):
+        self.logging.info("Opening publish channels")
         self.sensor_data_channel = connection.channel()
         self.sensor_data_channel.queue_declare(queue="sensor_data")
 
@@ -66,52 +74,79 @@ class RabbitMQGateway(IMessageQueuePublisher):
         self.publish_connection.close()
 
     def send_temperature_status(self, temperature: WaterTemperature):
+        if not self.sensor_data_channel:
+            return
+
         with self.publish_lock:
             self.sensor_data_channel.basic_publish(
                 routing_key="measurement.temperature", body=json.dumps({"temperature": temperature.value})
             )
 
     def send_ph_value(self, ph: PH):
+        if not self.sensor_data_channel:
+            return
+
         with self.publish_lock:
             self.sensor_data_channel.basic_publish(routing_key="measurement.ph", body=json.dumps({"ph": ph.value}))
 
     def send_fertilizer_level(self, ec: Conductivity):
+        if not self.sensor_data_channel:
+            return
+
         with self.publish_lock:
             self.sensor_data_channel.basic_publish(
                 routing_key="measurement.ec", body=json.dumps({"ec": ec.microsiemens_per_meter})
             )
 
     def send_pressure_status(self, pressure: Pressure):
+        if not self.sensor_data_channel:
+            return
+
         with self.publish_lock:
             self.sensor_data_channel.basic_publish(
                 routing_key="measurement.pressure", body=json.dumps({"pressure": pressure.bar})
             )
 
     def send_spray_message(self, index: int, duration: int):
+        if not self.event_data_channel:
+            return
+
         with self.publish_lock:
             self.event_data_channel.basic_publish(
                 routing_key=f"event.boxes.spray.{index}", body=json.dumps({"index": index, "duration": duration})
             )
 
     def send_ph_raised(self, amount: float):
+        if not self.event_data_channel:
+            return
+
         with self.publish_lock:
             self.event_data_channel.basic_publish(
                 routing_key=f"event.adjustment.ph.up", body=json.dumps({"amount": amount})
             )
 
     def send_ph_lowered(self, amount: float):
+        if not self.event_data_channel:
+            return
+
         with self.publish_lock:
             self.event_data_channel.basic_publish(
                 routing_key=f"event.adjustment.ph.down", body=json.dumps({"amount": amount})
             )
 
     def send_ec_lowered(self, amount: float):
+        if not self.event_data_channel:
+            return
+
         with self.publish_lock:
             self.event_data_channel.basic_publish(
                 routing_key=f"event.adjustment.ec.down", body=json.dumps({"amount": amount})
             )
 
     def send_ec_increased(self, amount_grow: float, amount_micro: float, amount_bloom: float):
+        if not self.event_data_channel:
+            return
+
         with self.publish_lock:
             self.event_data_channel.basic_publish(
                 routing_key=f"event.adjustment.ec.up",
@@ -121,17 +156,26 @@ class RabbitMQGateway(IMessageQueuePublisher):
             )
 
     def send_pressure_raised(self, pressure: Pressure):
+        if not self.event_data_channel:
+            return
+
         with self.publish_lock:
             self.event_data_channel.basic_publish(
                 routing_key=f"event.adjustment.pressure.up", body=json.dumps({"bar": pressure.bar})
             )
 
     def send_unexpected_pressure_drop(self, pressure_drop: float):
+        if not self.event_data_channel:
+            return
+
         with self.publish_lock:
             self.event_data_channel.basic_publish(
                 routing_key=f"event.exception.pressure.drop", body=json.dumps({"drop": pressure_drop})
             )
 
     def send_could_not_raise_pressure(self):
+        if not self.event_data_channel:
+            return
+
         with self.publish_lock:
             self.event_data_channel.basic_publish(routing_key=f"event.exception.pressure.not_increasing", body="")
