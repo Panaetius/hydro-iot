f4d89c9986909bdd4f4dbd14a43081d1e6adbd1d
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2022-02-18 20:50:10 +0100

    add lock for power output

--- hydro_iot/infrastructure/spray_gateway.py
+++ hydro_iot/infrastructure/spray_gateway.py
@@ -4,6 +4,7 @@ import inject
 import RPi.GPIO as GPIO
 
 from hydro_iot.domain.config import IConfig
+from hydro_iot.domain.system_state import SystemState
 from hydro_iot.services.ports.logging import ILogging
 from hydro_iot.services.ports.spray_gateway import ISprayGateway
 
@@ -11,6 +12,7 @@ from hydro_iot.services.ports.spray_gateway import ISprayGateway
 class SprayGateway(ISprayGateway):
     config = inject.attr(IConfig)
     _log = inject.attr(ILogging)
+    system_state = inject.attr(SystemState)
 
     def __init__(self) -> None:
         self._log.info("Init spray gateway")
@@ -20,6 +22,9 @@ class SprayGateway(ISprayGateway):
             GPIO.setup(box_pin, GPIO.OUT)
 
     def spray_box(self, index: int, duration: int):
-        GPIO.output(self.config.pins.box_spray_pins[index], GPIO.HIGH)
-        sleep(duration / 1000.0)
-        GPIO.output(self.config.pins.box_spray_pins[index], GPIO.LOW)
+        with self.system_state.power_output_lock:
+            try:
+                GPIO.output(self.config.pins.box_spray_pins[index], GPIO.HIGH)
+                sleep(duration / 1000.0)
+            finally:
+                GPIO.output(self.config.pins.box_spray_pins[index], GPIO.LOW)
